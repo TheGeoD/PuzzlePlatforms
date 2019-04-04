235913c01895de34bbf1f113b9b379051108bd9b
Dappr_Bones <Geoffdwade@gmail.com>
2019-04-04 07:07:27 -0400

    Finished Section 1

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -17,10 +17,23 @@ void UPuzzlePlatformsGameInstance::Init()
 
 void UPuzzlePlatformsGameInstance::Host()
 {
-	GEngine->AddOnScreenDebugMessage(0, 2.0f, FColor::Green, TEXT("Hosting")); 
+	GEngine->AddOnScreenDebugMessage(0, 2.0f, FColor::Green, TEXT("Hosting"));
+	GetWorld()->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
+
+// 	UWorld* World = GetWorld();
+// 	if(World)
+// 	{
+// 		World->ServerTravel(""); 
+// 	}
 }
 
 void UPuzzlePlatformsGameInstance::Join(const FString& Address)
 {
 	GEngine->AddOnScreenDebugMessage(0, 5.0f, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if(PlayerController)
+	{
+		PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute); 
+	}
 }
